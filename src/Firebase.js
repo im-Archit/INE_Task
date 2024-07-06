f4fea945a6c212c11b6049525e795c8b350c765c
Archit <109897872+im-Archit@users.noreply.github.com>
2024-07-06 23:07:43 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,13 +4,7 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClIWMNYL-2b_KNys3Neoi9ZIxwDxJqrmc",
-  authDomain: "swiggy-ae0b2.firebaseapp.com",
-  projectId: "swiggy-ae0b2",
-  storageBucket: "swiggy-ae0b2.appspot.com",
-  messagingSenderId: "776344570622",
-  appId: "1:776344570622:web:a137647425ea0747a233af",
-  measurementId: "G-SECCY47X2G",
+ //Apna Daal Lena 💕
 };
 
 firebase.initializeApp(firebaseConfig);
